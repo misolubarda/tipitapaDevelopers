088be915391f35100766bf5204eafb6e0dc6aafb
Miso Lubarda <miso.lubarda@gmail.com>
2022-02-19 15:23:24 -0600

    Enums

---
+++ Tipitapa/CountriesService.swift
@@ -0,0 +1,43 @@
+//
+//  Lesson02.swift
+//  Tipitapa
+//
+//  Created by Miso Lubarda on 18.02.22.
+//
+
+import Foundation
+
+class CountriesService {
+    func namesSortedAlphabetically() -> [String] {
+        let countries: [Country] = [.nicaragua, .costaRica, .mexico, .bolivia, .elSalvador, .argentina]
+
+        var countryNames: [String] = []
+
+        for country in countries {
+            let countryName = country.rawValue
+            countryNames.append(countryName)
+        }
+
+        countryNames.sort()
+
+        return countryNames
+    }
+
+    // homework
+    func allCountriesInOneString() -> String {
+        // 1. "Nicaragua❤️, Costa Rica, Mexico, Bolivia, El Salvador, Argentina are beautiful countries."
+        // 2. "Nicaragua❤️, Costa Rica, Mexico, Bolivia, El Salvador and Argentina are beautiful countries."
+
+        return ""
+    }
+}
+
+enum Country: String {
+    case nicaragua = "Nicaragua❤️"
+    case costaRica = "Costa Rica"
+    case mexico = "Mexico"
+    case bolivia = "Bolivia"
+    case elSalvador = "El Salvador"
+    case argentina = "Argentina"
+}
+
