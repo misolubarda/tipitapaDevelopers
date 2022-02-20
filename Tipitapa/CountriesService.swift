f06fcb449e364d33b95a07909d1af68cdd00971d
Miso Lubarda <miso.lubarda@gmail.com>
2022-02-20 15:25:32 -0600

    Finish the countries names homework partially

--- Tipitapa/CountriesService.swift
+++ Tipitapa/CountriesService.swift
@@ -25,7 +25,7 @@ class CountriesService {
     }
     // homework
    
-    func allCountriesInOneString() -> [String] {
+    func allCountriesInOneString() -> String {
         // 1. "Nicaragua❤️, Costa Rica, Mexico, Bolivia, El Salvador, Argentina are beautiful countries."
         // 2. "Nicaragua❤️, Costa Rica, Mexico, Bolivia, El Salvador and Argentina are beautiful countries."
         let beautifulCountries: [Country] = [.nicaragua,.costaRica,.mexico,.bolivia,.elSalvador]
@@ -36,10 +36,20 @@ class CountriesService {
             let beautifulCountriesName =  country.rawValue
             beautifulCountriesNames.append(beautifulCountriesName)
         }
-        let beautifulCountriesNamess = beautifulCountriesNames[0] + " " + "are beautiful countries"
-        return beautifulCountriesNames
 
+        var finalText: String = ""
+        for countryName in beautifulCountriesNames {
+            finalText = finalText + countryName + ", " // "Nicaragua❤️, " -> "Nicaragua❤️, Costa Rica, "
+        }
+        // finalText -> "Nicaragua❤️, Costa Rica, Mexico, Bolivia, El Salvador, Argentina, "
+        //                      we need to remove the last 2 characters: comma and space ↑↑
 
+        finalText.removeLast()
+        finalText.removeLast()
+
+        // finalText -> "Nicaragua❤️, Costa Rica, Mexico, Bolivia, El Salvador, Argentina"
+
+        return finalText
     }
 }
 
